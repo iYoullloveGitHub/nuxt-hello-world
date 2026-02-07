34f64eb2c8a2fd8639348003b349ab28f8fec8cb
chen-chen <1071148025@qq.com>
2026-02-07 19:57:30 +0800

    fix-base-path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  
+  // GitHub Pages 部署配置
+  app: {
+    baseURL: '/nuxt-hello-world/', // GitHub Pages 子路径
+    buildAssetsDir: '/_nuxt/',     // 静态资源路径
+  }
 })
